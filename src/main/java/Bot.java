c3c4e5cf81641d22fa2ecf739262bdfa359ffa6e
LetsDoIt291 <80558055+LetsDoIt291@users.noreply.github.com>
2022-06-21 13:39:18 -0400

    1.0

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -7,15 +7,25 @@ import net.dv8tion.jda.api.utils.ChunkingFilter;
 import net.dv8tion.jda.api.utils.MemberCachePolicy;
 
 import javax.security.auth.login.LoginException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class Bot {
 
     static EventWaiter waiter = new EventWaiter();
 
     static boolean botDebug = false;
 
-    public static void main(String[] args) throws LoginException {
+    public static void main(String[] args) throws LoginException, FileNotFoundException {
         Storage.clearOpenTicket();
         Storage.clearActiveAppeals();
+
+        //So I stop exposing my token and don't have to keep resetting it
+        File myObj = new File("C:\\Users\\Letsd\\Desktop\\Token.txt");
+        Scanner myReader = new Scanner(myObj);
+
+        String token = myReader.nextLine();
 
         CommandClientBuilder client = new CommandClientBuilder();
         client.setOwnerId("395011406426144778");
@@ -24,7 +34,7 @@ public class Bot {
         client.useHelpBuilder(false);
         client.setActivity(Activity.listening("-help"));
 
-        JDABuilder jda = JDABuilder.createDefault("")
+        JDABuilder jda = JDABuilder.createDefault(token)
                 .setChunkingFilter(ChunkingFilter.ALL)
                 .setMemberCachePolicy(MemberCachePolicy.ALL)
                 .enableIntents(GatewayIntent.GUILD_MEMBERS)
